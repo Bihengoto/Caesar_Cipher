c85efe06cd63007a3a034fd98fd2d96fdaf7d937
Bihengoto <isaacbihengoto@gmail.com>
2019-04-09 16:00:34 +0300

    fixed a number of bugs in the file

--- src/main/java/cipher/encrypt.java
+++ src/main/java/cipher/encrypt.java
@@ -1,28 +1,28 @@
-//package cipher;
-//
-//public class encrypt {
-//    public static String encrypt(String text, int key)
-//    {
-//        String result= "";
-//
-//        for (int i=0; i<text.length(); i++)
-//        {
-//            if (Character.isUpperCase(text.charAt(i)))
-//            {
-//                char ch = (char)(((int)text.charAt(i) +
-//                        key - 65) % 26 + 65);
-//                result += ch;
-//            }
-//            else if (Character.isLowerCase(text.charAt(i)))
-//            {
-//                char ch = (char)(((int)text.charAt(i) +
-//                        key - 97) % 26 + 97);
-//                result += ch;
-//            } else {
-//                char ch = (char)((int)text.charAt(i));
-//                result += ch;
-//            }
-//        }
-//        return result;
-//    }
-//}
+package cipher;
+
+public class encrypt {
+    public static String encrypt(String text, int key)
+    {
+        String result= "";
+
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                        key - 65) % 26 + 65);
+                result += ch;
+            }
+            else if (Character.isLowerCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                        key - 97) % 26 + 97);
+                result += ch;
+            } else {
+                char ch = (char)((int)text.charAt(i));
+                result += ch;
+            }
+        }
+        return result;
+    }
+}
