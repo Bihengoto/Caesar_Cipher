3c323df31dcb31b79c3ea72ad0a9ec523c842522
Bihengoto <isaacbihengoto@gmail.com>
2019-04-09 15:57:46 +0300

    fixed a number of bugs in the file

--- src/main/java/cipher/decrypt.java
+++ src/main/java/cipher/decrypt.java
@@ -1,25 +1,25 @@
-//package cipher;
-//
-//public class decrypt {
-//    public static String decrypt(String text, int key)
-//    {
-//        String result= "";
-//
-//        for (int i=0; i<text.length(); i++)
-//        {
-//            if (Character.isUpperCase(text.charAt(i)))
-//            {
-//                char ch = (char)(((int)text.charAt(i) -
-//                        key - 65) % 26 + 65);
-//                result += ch;
-//            }
-//            else
-//            {
-//                char ch = (char)(((int)text.charAt(i) -
-//                        key - 97) % 26 + 97);
-//                result += ch;
-//            }
-//        }
-//        return result;
-//    }
-//}
+package cipher;
+
+public class decrypt {
+    public static String decrypt(String text, int key)
+    {
+        String result= "";
+
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) -
+                        key - 65) % 26 + 65);
+                result += ch;
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) -
+                        key - 97) % 26 + 97);
+                result += ch;
+            }
+        }
+        return result;
+    }
+}
